a0ee1b542d354b7143fde7256bc2c759d3a1cbc6
Damian Dziaduch <damian@dziaduch.pl>
2025-02-01 14:58:10 +0100

    Introduce framework & adapters

--- src/Controller/ProfilesController.php
+++ src/Controller/ProfilesController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Framework\Symfony\Component\Security\Core\User;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
