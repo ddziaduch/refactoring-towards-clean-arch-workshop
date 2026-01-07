99ac8e8fa9294c8e3835453b4d5eda0b10f14e5e
Damian Dziaduch <damian@dziaduch.pl>
2026-01-07 20:29:39 +0100

    Improve installation

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+docker compose up -d
+docker compose exec app composer i
+docker compose exec app bin/console doctrine:database:create -etest
+docker compose exec app bin/console doctrine:migrations:migrate -etest -n
+docker compose exec app vendor/bin/phpunit
