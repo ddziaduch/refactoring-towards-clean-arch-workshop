c45fe1f6a6c2ca3be3a0a00a4bf9d24761037edf
Damian Dziaduch <damian@dziaduch.pl>
2026-04-22 11:45:28 +0200

    Cover with tests

---
+++ tests/CreateCommentTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Entity\Comment;
+use PHPUnit\Framework\Attributes\Test;
+
+final class CreateCommentTest extends BaseTestCase
+{
+    #[Test]
+    public function happyPath(): void
+    {
+        $this->login();
+
+        $this->client->jsonRequest(
+            method: 'POST',
+            uri: '/api/articles/test-article-user-first/comments',
+            parameters: [
+                'comment' => [
+                    'body' => 'Hello world!'
+                ],
+            ],
+        );
+
+        self::assertResponseIsSuccessful();
+
+        $entityManager = self::getContainer()->get('doctrine.orm.default_entity_manager');
+        $comments = $entityManager->getRepository(Comment::class)->findAll();
+        self::assertCount(1, $comments);
+        $comment = $comments[0];
+        self::assertSame('Hello world!', $comment->body);
+    }
+
+    public function test404(): void
+    {
+        $this->login();
+
+        $this->client->jsonRequest(
+            method: 'POST',
+            uri: '/api/articles/non-existing-article/comments',
+            parameters: [
+                'comment' => [
+                    'body' => 'Hello world!'
+                ],
+            ],
+        );
+
+        self::assertResponseStatusCodeSame(404);
+    }
+
+    public function testInvalidPayload(): void
+    {
+        $this->login();
+
+        $this->client->jsonRequest(
+            method: 'POST',
+            uri: '/api/articles/test-article-user-first/comments',
+            parameters: [
+                'yooo' => [
+                    'mama' => '???'
+                ],
+            ],
+        );
+
+        self::assertResponseStatusCodeSame(400);
+    }
+}
