6cf665186c8cbe691c3edcadaad6c68f5749c077
Damian Dziaduch <damian@dziaduch.pl>
2026-04-22 15:19:12 +0200

    New primary adapter/controller

---
+++ srcClean/Adapters/Primary/CreateCommentController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use Clean\Application\Ports\Secondary\CommentDtoFinder;
+use Clean\Domain\Entities\Article;
+use Clean\Domain\Entities\Comment;
+use App\Entity\User;
+use Clean\Application\Exceptions\ArticleNotFoundException;
+use Clean\Application\Ports\Primary\CreateArticleCommentUseCaseInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+#[AsController]
+class CommentController
+{
+    #[Route('/api/articles/{slug}/comments', name: 'CreateArticleComment', methods: ['POST'])]
+    public function createArticleComment(
+        string $slug,
+        #[CurrentUser] User $user,
+        Request $request,
+        CreateArticleCommentUseCaseInterface $useCase,
+        CommentDtoFinder $dtoFinder,
+    ) {
+        $commentBody = json_decode($request->getContent(), true)['comment']['body']
+            ?? throw new BadRequestHttpException('Comment is missing');
+
+        try {
+            $commentId = $useCase->run(
+                $slug,
+                $commentBody,
+                $user,
+            );
+        } catch (ArticleNotFoundException $e) {
+            throw new NotFoundHttpException($e->getMessage(), $e);
+        }
+
+        $comment = $dtoFinder->findById($commentId);
+
+        return new JsonResponse([
+            'comment' => [
+                'author' => [
+                    'bio' => $user->bio,
+                    'following' => $user->following->contains($user),
+                    'image' => $user->image,
+                    'username' => $user->username,
+                ],
+                'body' => $comment->body,
+                'createdAt' => $comment->createdAt->format(DATE_ATOM),
+                'id' => $comment->id,
+                'updatedAt' => $comment->updatedAt->format(DATE_ATOM),
+            ],
+        ]);
+    }
+
+    #[Route('/api/articles/{slug}/comments', name: 'GetArticleComments', methods: ['GET'])]
+    public function getArticleComments(
+        string $slug,
+        #[CurrentUser] ?User $user,
+        EntityManagerInterface $entityManager,
+    ) {
+        $article = $entityManager->getRepository(Article::class)->findOneBy(['slug' => $slug]);
+
+        return new JsonResponse([
+            'comments' => $article->comments->map(
+                function (Comment $comment) use ($user): array {
+                    return [
+                        'author' => [
+                            'bio' => $comment->author->bio,
+                            'following' => $user && $comment->author->following->contains($user),
+                            'image' => $user->image,
+                            'username' => $user->username,
+                        ],
+                        'body' => $comment->body,
+                        'createdAt' => $comment->createdAt->format(DATE_ATOM),
+                        'id' => $comment->id(),
+                        'updatedAt' => $comment->updatedAt->format(DATE_ATOM),
+                    ];
+                },
+            )->toArray(),
+        ]);
+    }
+
+    #[Route('/api/articles/{slug}/comments/{id}', name: 'DeleteArticleComment', methods: ['DELETE'])]
+    public function deleteArticleComment(
+        string $slug,
+        int $id,
+        #[CurrentUser] User $user,
+        EntityManagerInterface $entityManager,
+    ): Response {
+        $comment = $entityManager->find(Comment::class, $id);
+
+        if (!$comment || $comment->article->slug !== $slug) {
+            throw new NotFoundHttpException('Comment not found');
+        }
+
+        if ($comment->author !== $user) {
+            throw new AccessDeniedHttpException('You are not allowed to delete this comment');
+        }
+
+        $entityManager->remove($comment);
+        $entityManager->flush();
+
+        return new Response();
+    }
+}
